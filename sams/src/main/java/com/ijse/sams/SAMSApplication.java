c71f0934d5a05dc0cff3f0db9554a96de0691e47
Lihini-Lochana <lihinilochana18@gmail.com>
2025-07-18 15:13:19 +0530

    JavaFX and Hibernate Setup

---
+++ sams/src/main/java/com/ijse/sams/SAMSApplication.java
@@ -0,0 +1,18 @@
+package com.ijse.sams;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SAMSApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ sams/src/main/java/com/ijse/sams/entity/User.java
@@ -0,0 +1,5 @@
+package com.ijse.sams.entity;
+
+public class User {
+   
+}
